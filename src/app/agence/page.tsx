1390f8f8171f76db6f183d5d71723c28e64415d2
codeAyush05 <ayushh.dev05@gmail.com>
2026-01-10 23:37:37 +0530

    updated

--- src/app/agence/page.tsx
+++ src/app/agence/page.tsx
@@ -4,15 +4,16 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import Lenis from "@studio-freight/lenis";
-import { useEffect, useRef } from "react";
+import { JSX, useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/all";
 
-export default function Agence() {
-  const imageDivRef = useRef(null);
+export default function Agence(): JSX.Element {
+  const imageDivRef = useRef<HTMLDivElement | null>(null);
+  const imageRef = useRef<HTMLImageElement | null>(null);
 
   gsap.registerPlugin(ScrollTrigger);
 
-  const imageArr = [
+  const imageArr: string[] = [
     "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
     "/images/CAMILLE_480X640_2-480x640.jpg",
     "/images/ChantalG_480x640-480x640.jpg",
@@ -28,9 +29,9 @@ export default function Agence() {
     "/images/HugoJoseph_480x640-480x640.jpg",
   ];
 
-  const imageRef = useRef(null);
-
   useGSAP(() => {
+    if (!imageDivRef.current || !imageRef.current) return;
+
     gsap.to(imageDivRef.current, {
       scrollTrigger: {
         trigger: imageDivRef.current,
@@ -40,61 +41,63 @@ export default function Agence() {
         scrub: true,
         // markers: true,
 
-        onUpdate: (elem) => {
-          let imageIndex;
-          if (elem.progress < 1) {
-            imageIndex = Math.floor(elem.progress * imageArr.length);
+        onUpdate: (self: ScrollTrigger) => {
+          let imageIndex: number;
+
+          if (self.progress < 1) {
+            imageIndex = Math.floor(self.progress * imageArr.length);
           } else {
             imageIndex = imageArr.length - 1;
           }
 
-          imageRef.current.src = imageArr[imageIndex];
+          if (imageRef.current) {
+            imageRef.current.src = imageArr[imageIndex];
+          }
         },
       },
     });
-  });
+  }, []);
 
   useEffect(() => {
-    // Initialize Lenis
     const lenis = new Lenis({
       smoothWheel: true,
     });
 
-    // Sync Lenis with GSAP ScrollTrigger
-    function raf(time: DOMHighResTimeStamp) {
+    const raf = (time: number): void => {
       lenis.raf(time);
-      ScrollTrigger.update(); // important!
+      ScrollTrigger.update();
       requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
+    };
 
-    // Example GSAP animation with ScrollTrigger
+    requestAnimationFrame(raf);
 
     return () => {
       lenis.destroy();
     };
   }, []);
+
   return (
     <>
       <div className="text-black">
-        <div className="section-1 relative text-black h-[135vh] flex justify-center items-end w-full ">
+        <div className="section-1 relative text-black h-[135vh] flex justify-center items-end w-full">
           <div
             ref={imageDivRef}
-            className="overflow-hidden absolute h-[45vh]  top-30 left-95 w-[15vw] rounded-3xl ">
+            className="overflow-hidden absolute h-[45vh] top-30 left-95 w-[15vw] rounded-3xl">
             <img
               ref={imageRef}
               src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
               alt="man"
-              className="object-cover w-full h-full  object-center"
+              className="object-cover w-full h-full object-center"
             />
           </div>
 
-          <h1 className="text-[45vh]   leading-none  uppercase text-center z-20 ">
+          <h1 className="text-[45vh] font-bold leading-none uppercase text-center z-20">
             Soixan7e<div className="-mt-7">Douze</div>
           </h1>
         </div>
-        <div className="section-2  w-full flex justify-end text-black">
-          <p className=" z-20 font-semibold pr-5 leading-14 w-[60%] text-[40px] ">
+
+        <div className="section-2 w-full flex justify-end text-black">
+          <p className="z-20 font-semibold pr-5 leading-14 w-[60%] text-[40px]">
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité
@@ -108,30 +111,27 @@ export default function Agence() {
         </div>
 
         <div className="section-3 pt-60 gap-50 flex flex-col w-full">
-          <div className="w-[50%] flex text-xl pl-50  justify-between  ">
+          <div className="w-[50%] flex text-xl pl-50 justify-between">
             <p>Expertise</p>
             <p className="w-30">Stratégie Publicité Branding Design Contenu</p>
           </div>
-          <div className="w-full flex text-[18px]  font-semibold gap-12   justify-center  ">
-            <p className=" w-80">
+
+          <div className="w-full flex text-[18px] font-semibold gap-12 justify-center">
+            <p className="w-80">
               Nos projets_ naissent dans l’humilité, grandissent dans la
               curiosité et vivent grâce à la créativité sous toutes ses formes.
             </p>
-            <p className=" w-85">
+            <p className="w-85">
               Notre création_ bouillonne dans un environnement où le talent a le
               goût d’exploser. Où on se sent libre d’être la meilleure version
               de soi-même.
             </p>
-            <p className=" w-60">
+            <p className="w-60">
               Notre culture_ c’est l’ouverture aux autres. Point. Tout
-              l’équipage participe à bâtir une agence dont on est fiers.{" "}
+              l’équipage participe à bâtir une agence dont on est fiers.
             </p>
           </div>
         </div>
-
-        {/* <div className="bg-red-800">
-          <img src="" alt="" />
-        </div> */}
       </div>
     </>
   );
