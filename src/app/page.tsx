ad85972d7fa088d8075d0842e9a0cf8bd6fadfda
codeAyush05 <ayushh.dev05@gmail.com>
2026-01-11 23:27:19 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,50 @@
 /** @format */
+"use client";
 import Image from "next/image";
 import "./globals.css";
 import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
 
 export default function Home() {
+  const parentStair = useRef(null);
+  useGSAP(function () {
+    const tl = gsap.timeline();
+    tl.to(parentStair.current, {
+      display: "block",
+    });
+    tl.from(".stair", {
+      height: 0,
+      stagger: {
+        amount: -0.25,
+      },
+    });
+    tl.to(".stair", {
+      y: "100%",
+      stagger: {
+        amount: -0.25,
+      },
+    });
+    tl.from(parentStair.current, {
+      display: "none",
+    });
+    tl.to(".stair", {
+      y: "0%",
+    });
+  });
   return (
     <>
-      <div className=" h-[110vh] w-full ">
+      <div className=" h-[110vh]  w-full ">
+        <div ref={parentStair} className="h-screen fixed top-0 z-10  w-full">
+          <div className="h-full w-full flex">
+            <div className="stair h-full w-1/5 bg-black "></div>
+            <div className="stair h-full w-1/5 bg-black "></div>
+            <div className="stair h-full w-1/5 bg-black "></div>
+            <div className="stair h-full w-1/5 bg-black "></div>
+            <div className="stair h-full w-1/5 bg-black "></div>
+          </div>
+        </div>
         <video
           autoPlay
           muted
