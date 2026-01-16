c7d2f5c9dcfa00ca92ddb88fe8227a6236f9c949
codeAyush05 <ayushh.dev05@gmail.com>
2026-01-16 23:13:55 +0530

    updatd

--- src/app/components/fullNavbar/page.tsx
+++ src/app/components/fullNavbar/page.tsx
@@ -1,13 +1,36 @@
 /** @format */
 
+"use client";
+// import React from "react";
 export default function fullScreenNavbar() {
   return (
     <>
       <div className="h-screen w-full bg-black font-bold  ">
-        <div className="h-1/6  ">1</div>
+        <div className="h-1/6  "></div>
         <div className="h-1/6 border-t-1 border-white/50 flex justify-center items-center text-[90px]  ">
-          <h1>PROJETS</h1>
-          <div></div>
+          {/* <h1>PROJETS</h1> */}
+          <marquee behavior="scroll" direction="left" scrollamount="30">
+            <div className="flex w-full justify-center items-center  h-1/6">
+              Pour tout voir&nbsp;
+              <div className=" w-90 h-17   rounded-[30px] overflow-hidden bg-white">
+                {" "}
+                <img
+                  className="object-cover h-full w-full object-center"
+                  src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b"
+                  alt="bag"
+                />
+              </div>
+              &nbsp; Pour tout voir&nbsp;
+              <div className=" w-50 h-17   rounded-[30px] overflow-hidden bg-white">
+                {" "}
+                <img
+                  className="object-cover h-full w-full object-center"
+                  src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93"
+                  alt="t-shirt"
+                />
+              </div>
+            </div>
+          </marquee>
         </div>
         <div className="h-1/6 border-t-1 border-white/50 flex justify-center items-center text-[90px]  ">
           <h1>AGENCE</h1>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-        <Stairs />
+        {/* <Stairs /> */}
         <FullScreenNavbar />
-        {children}
+        {/* {children} */}
       </body>
     </html>
   );
