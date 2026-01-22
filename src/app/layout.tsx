7fc08b5aa48eff1fe888c00ea4b0ca66d08b5a3f
codeAyush05 <ayushh.dev05@gmail.com>
2026-01-22 22:39:05 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navbar />
-        <Stairs />
+        {/* <Navbar />
+        <Stairs /> */}
         <FullScreenNavbar />
         {children}
       </body>
