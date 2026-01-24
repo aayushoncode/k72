bf52fbd3670f0dd61b370dcc26818936366eae45
codeAyush05 <ayushh.dev05@gmail.com>
2026-01-24 22:32:51 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,8 @@ export default function RootLayout({
         {/* <Navbar />
         <Stairs /> */}
         <FullScreenNavbar />
-        {children}
-      </body>
+        {/* {children}      */}
+         </body>
     </html>
   );
 }
